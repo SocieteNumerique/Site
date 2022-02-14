4c1bd00e2181f5ec37fa9bdb0731269134e6ae3c
Camille <camille@telescoop.fr>
2022-02-14 15:37:44 +0100

    [+] create rss feed

---
+++ home/feeds.py
@@ -0,0 +1,36 @@
+from django.contrib.syndication.views import Feed
+
+from home.models import News, NewsListPage
+from home.templatetags.scheme_tags import news_page_url
+
+
+class LatestNewsFeed(Feed):
+    title = "News"
+    link = "/rss/actualite"
+    description = "Notifications lors de modification ou d'ajout de nouveaux évènement et actualité dans le comtpte de Société Numérique."
+    description_template = "feeds/news.html"
+
+    def items(self):
+        return News.objects.order_by("-publication_date")[:5]
+
+    def item_title(self, item):
+        return item.name
+
+    def item_link(self, item):
+        news_list_page = NewsListPage.objects.filter(locale_id=item.locale_id).get()
+        return news_page_url(news_list_page, item)
+
+    def get_context_data(self, **kwargs):
+        """
+        {'obj': item, 'site': current_site} as the super context.
+        """
+        context = super().get_context_data(**kwargs)
+        context["image_url"] = (
+            context["site"].domain + context["obj"].image.file.url
+            if context["obj"].image
+            else None
+        )
+        context["scheme_names"] = (
+            context["obj"].schemes.all().values_list("name", flat=True)
+        )
+        return context
--- sonum/urls.py
+++ sonum/urls.py
@@ -7,6 +7,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
+from home.feeds import LatestNewsFeed
 from search import views as search_views
 from sonum.views import clear_cache
 from tweets import views as tweets_views
@@ -21,6 +22,7 @@ urlpatterns = [
     path("tweets/", tweets_views.get_tweets, name="get_tweets"),
     path("backup/", include("telescoop_backup.urls")),
     path("cache/clear", clear_cache),
+    path("latest/feed/", LatestNewsFeed()),
 ]
 
 
